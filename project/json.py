4413eff2e9985ae8def8ba2a3e340207d001136f
Daniel Shin <95920372+daniel-dk-shin@users.noreply.github.com>
2023-06-04 22:42:53 +1000

    Added comments regarding fixed Sonarlint warnings in json.py

--- project/json.py
+++ project/json.py
@@ -8,7 +8,7 @@ json = Blueprint('json', __name__)
 
 #JSON APIs to view Restaurant Information
 @json.route('/restaurant/<restaurant_id>/menu/JSON')
-def restaurant_menu_json(restaurant_id):
+def restaurant_menu_json(restaurant_id): # Renamed function 'restaurantMenuJSON' to match regular expression
     query = text('SELECT * FROM menu_item WHERE restaurant_id = :restaurant_id')
     items = db.session.execute(query, {'restaurant_id': restaurant_id})
     # Query previously depended on user-provided value: which used string formatting for restaurant_id
@@ -18,7 +18,7 @@ def restaurant_menu_json(restaurant_id):
 
 
 @json.route('/restaurant/<restaurant_id>/menu/<int:menu_id>/JSON')
-def menu_item_json(restaurant_id, menu_id):
+def menu_item_json(restaurant_id, menu_id): # Renamed function 'menuItemJSON' to match regular expression
     query = text('SELECT * FROM menu_items WHERE id = :menu_id LIMIT 1')
     menu_item = db.session.execute(query, {'menu_id': menu_id}).fetchone()
     # Query previously depended on user-provided value: which used string formatting for menu_id
@@ -27,7 +27,7 @@ def menu_item_json(restaurant_id, menu_id):
     return pyjs.dumps(items_list)
 
 @json.route('/restaurant/JSON')
-def restaurants_json():
+def restaurants_json(): # Renamed function 'restaurantsJSON' to match regular expression
     restaurants = db.session.execute(text('select * from restaurant'))
     rest_list = [ r._asdict() for r in restaurants ]
     return pyjs.dumps(rest_list)
