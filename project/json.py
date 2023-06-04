35be0c825156ee7eb2532ec7f2eef1ca625c4dec
Daniel Shin <95920372+daniel-dk-shin@users.noreply.github.com>
2023-06-04 22:49:15 +1000

    Edited/improved comments regarding fixed CodeQL security issues in json.py

--- project/json.py
+++ project/json.py
@@ -11,8 +11,7 @@ json = Blueprint('json', __name__)
 def restaurant_menu_json(restaurant_id): # Renamed function 'restaurantMenuJSON' to match regular expression
     query = text('SELECT * FROM menu_item WHERE restaurant_id = :restaurant_id')
     items = db.session.execute(query, {'restaurant_id': restaurant_id})
-    # Query previously depended on user-provided value: which used string formatting for restaurant_id
-    # Vulnerability to SQL Injection Attack fixed through parameters and placeholders
+    # Fixed CodeQL Security Issue: Prior query used string formatting for user-provided value 'restaurant_id'
     items_list = [ i._asdict() for i in items ]
     return pyjs.dumps(items_list)
 
@@ -21,8 +20,7 @@ def restaurant_menu_json(restaurant_id): # Renamed function 'restaurantMenuJSON'
 def menu_item_json(restaurant_id, menu_id): # Renamed function 'menuItemJSON' to match regular expression
     query = text('SELECT * FROM menu_items WHERE id = :menu_id LIMIT 1')
     menu_item = db.session.execute(query, {'menu_id': menu_id}).fetchone()
-    # Query previously depended on user-provided value: which used string formatting for menu_id
-    # Vulnerability to SQL Injection Attack fixed through parameters and placeholders
+    # Fixed CodeQL Security Issue: Pior query used string formatting for user-provided value 'menu_id'
     items_list = [ i._asdict() for i in menu_item ]
     return pyjs.dumps(items_list)
 
